ac423573c81dd0b679ceaeab939314e5091f9ae5
toufiglu <a656106458@gmail.com>
2024-05-07 21:33:47 -0700

    Both models are cleaned and checked.

---
+++ codes/parameters.R
@@ -0,0 +1,20 @@
+RESET_MODELS = F
+get_path <- function(filename) return(paste0("", filename))
+result.panel.size = 0.98
+duo.panel.key <- unit(0.2, 'cm')
+SET_SEED = T
+cues <- c("VOT_centered", "f0_centered")
+prior_kappa.plot = 4^(1:6) 
+prior_nu.plot = 4^(1:6) 
+n.subject = 1
+example_label = "Cue_reweighting"
+# set parameter ranges considered by the optimization algorithm
+## parameters for representations model
+range.prior_kappa <- c(1, 10000)
+range.prior_nu <- c(4, 10000) 
+## parameters for bias model
+range.lapse_rate <- c(0, 1)
+range.beta_pi <- c(10^-3, 10^2)
+## parameters for normalization model
+range.prior_kappa.normalization <- c(1, 10000)
+categories.IH <- c("/b/", "/p/")
